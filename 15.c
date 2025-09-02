f67bb9aba2db2ac4bbee61c1959809a97a242a14
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-09-02 21:29:30 +0530

    struct in c

---
+++ 15.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+struct Student {
+    int marks;
+    char name[15];
+    int age;
+};
+
+ int main(){
+// we will learn about structures in cpp which we will also use in c language here 
+int arr[]= {1,2,3,4,5,6,7};
+printf("%d",*(arr+3));
+ // some concepts of pointers
+
+struct Student s1  = {93, "sarthak", 20};
+
+}
