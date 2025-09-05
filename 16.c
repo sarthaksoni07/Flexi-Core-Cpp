dd842656bb4931b1774255ad49db8b071b673bfd
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-09-05 20:00:00 +0530

    usage of structure in c language

--- 16.c
+++ 16.c
@@ -1,7 +1,16 @@
 //struct in c language
 #include<stdio.h>
  
-int main(){
+struct student
+{
+    int marks;
+    char name[15];
+    float height;
+};
 
+int main(){
+struct student s1;
+s1.marks=30;
+s1.height = 178.5;
 return 0;
 }
