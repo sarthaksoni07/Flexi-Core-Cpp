44c6a8beef660f3b6ec4798279e9865e9f329c6b
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-09-06 20:00:00 +0530

    important c string functions

--- 16.c
+++ 16.c
@@ -14,3 +14,25 @@ s1.marks=30;
 s1.height = 178.5;
 return 0;
 }
+
+//  String Handling Functions (<string.h>)
+
+// Since strings are just char arrays in C, these functions save a LOT of effort.
+
+// strcpy(dest, src) → Copy string.
+
+// strncpy(dest, src, n) → Copy at most n chars (safer).
+
+// strlen(str) → Find length of a string.
+
+// strcmp(s1, s2) → Compare 2 strings (returns 0 if equal).
+
+// strncmp(s1, s2, n) → Compare first n chars.
+
+// strcat(s1, s2) → Concatenate (append) s2 at end of s1.
+
+// strncat(s1, s2, n) → Append only n chars.
+
+// strchr(str, ch) → Find first occurrence of character.
+
+// strstr(str, sub) → Find first occurrence of substring
