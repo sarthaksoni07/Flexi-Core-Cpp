48cc1f2d381f5af88e80bcdcd37122cbd21e295a
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-09-08 20:00:00 +0530

    pointer to struct

--- 17.c
+++ 17.c
@@ -5,15 +5,18 @@ struct Student {
 };
 
 int main() {
-    struct Student s1;       // normal struct
+    struct Student s1,s2;       // normal struct
     struct Student *ptr;     // pointer to struct
+    struct Student *ptr1;     // pointer to struct
 
     ptr = &s1;               // point to s1
-
+    ptr1 = &s2;
     // accessing members using pointer
-    (*ptr).roll = 20;
-    (*ptr).marks = 88.5;
-
+    (*ptr).roll = 20;// dereferencing takin place inside the bracket 
+    (*ptr).marks = 88.5;// it mean go to the struct stored at ptr and then find out the marks attribute
+(*ptr1).roll= 21;
+(*ptr1).marks= 70;
     printf("Roll: %d, Marks: %.2f\n", s1.roll, s1.marks);
+    printf("Roll: %d, Marks: %.2f\n", s2.roll, s2.marks);
     return 0;
 }
