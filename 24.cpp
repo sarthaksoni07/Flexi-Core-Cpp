809e6709f7b466435539140a8b0a6ef718f81262
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-09-17 20:00:00 +0530

    inline function practice set

--- 24.cpp
+++ 24.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 // revision for datatype .cpp 
 inline int f(int a){return a*5;}
+inline int area_c(int r){return 22/7*(r^2);}
+inline int area_r(int s,int h){return s*h;}
+
+inline int defaul(int a, int b){}
 int main()
 { // inline function 
 cout<<f(4)<<endl;
+cout<<area_c(19)<<endl;
+cout<<area_r(19,10)<<endl;
 return 0;
 }
