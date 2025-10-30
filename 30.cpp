0a6095aec07dd3f745dc5b1a32a94df3c2fc6c30
Sarthak <sarthaksoni07@gmail.com>
2025-10-30 20:00:00 +0530

    data types in c++

--- 30.cpp
+++ 30.cpp
@@ -1,1 +1,13 @@
 // quickk recap of the concepts for the end terms revision 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"hello world"<<endl;
+    int a;
+    bool b;
+    double c;
+    char d;
+    // these are the basic datatypes.
+}
