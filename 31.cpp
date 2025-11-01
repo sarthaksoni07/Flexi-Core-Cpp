4605408f18d965c061de856947fa548ee92816a2
Sarthak <sarthaksoni07@gmail.com>
2025-11-01 20:00:00 +0530

    if else

---
+++ 31.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // flow statemtns
+    int a;
+    cin>>a;
+    if (a > 5)
+    {
+        cout << a << endl;
+    }
+    else
+    {
+        cout << "less than 5" << endl;
+    }
+
+ 
+
+    return 0;
+}
