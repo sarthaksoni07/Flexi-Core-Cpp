018b94223f063b3049a96ca3c2ff7cdec7400e22
Sarthak <sarthaksoni07@gmail.com>
2025-11-02 20:00:00 +0530

    switch statement

--- 31.cpp
+++ 31.cpp
@@ -15,7 +15,16 @@ int main()
         cout << "less than 5" << endl;
     }
 
- 
+       switch(a){
+        case 1: 
+        cout<<"the number is 1"<<endl;
+        break;
+
+        case 2:
+        cout<<"the number is 2"<<endl;
+        break;
+        
+    }
 
     return 0;
 }
