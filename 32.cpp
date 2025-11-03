284865e04b42ca3ea2df8d2405776e05cf0825d6
Sarthak <sarthaksoni07@gmail.com>
2025-11-03 20:00:00 +0530

    dynamic memory allocation

--- 32.cpp
+++ 32.cpp
@@ -4,7 +4,14 @@ using namespace std;
  
 int main()
 {
+    int a= 5;
+    int* ptr =&a;
     
- 
+    cout<<"adress of a -  "<<ptr<<endl<<"value of a - "<<*ptr;
+
+    int *ptrr = new int; //dynamic memory integer
+    delete ptrr;
+    int *array = new int[10];
+    delete[] array;
     return 0;
 }
