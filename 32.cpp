70bb1125cba9f3e4b5e882953fb30bb95489d354
Sarthak <sarthaksoni07@gmail.com>
2025-11-04 20:00:00 +0530

    dynamic memory allocation

--- 32.cpp
+++ 32.cpp
@@ -7,11 +7,15 @@ int main()
     int a= 5;
     int* ptr =&a;
     
-    cout<<"adress of a -  "<<ptr<<endl<<"value of a - "<<*ptr;
+    cout<<"adress of a -  "<<ptr<<endl<<"value of a - "<<*ptr<<endl;
 
     int *ptrr = new int; //dynamic memory integer
-    delete ptrr;
-    int *array = new int[10];
-    delete[] array;
+    *ptrr = 5;
+    cout<<*ptrr<<endl;
+    // delete ptrr;
+    int *array = new int[10]{1,2,3,4};
+    cout<<array[0]<<endl;
+
+    // delete[] array;
     return 0;
 }
